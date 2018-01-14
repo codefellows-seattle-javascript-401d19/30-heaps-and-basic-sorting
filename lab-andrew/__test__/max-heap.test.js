950c90de3473eeb0d77764f749f1558a82d08b83
Andrew Bloom <blooma116@gmail.com>
2018-01-14 00:13:41 -0800

    wrote tests and functionality for extractMaximum function in max-heap

--- lab-andrew/__test__/max-heap.test.js
+++ lab-andrew/__test__/max-heap.test.js
@@ -3,7 +3,7 @@
 const MaxHeap = require('../model/max-heap');
 
 describe('tests for max-heap.js', () => {
-  test('testing that max-heap stores maximum value in root position', () => {
+  test('testing that insert behaves properly and max-heap stores maximum value in internal array at index 0, also testing that peek returns the max value in the heap', () => {
     let testHeap = new MaxHeap();
     expect(testHeap.peek()).toBeNull();
     testHeap.insert(5);
@@ -12,5 +12,25 @@ describe('tests for max-heap.js', () => {
     expect(testHeap.peek()).toEqual(5);
     testHeap.insert(6);
     expect(testHeap.peek()).toEqual(6);
+  });
+
+  test('testing that extractMaximum method properly returns max value in the max-heap and removes that value', () => {
+    let testHeap = new MaxHeap();
+    testHeap.insert(5);
+    expect(testHeap.extractMaximum()).toEqual(5);
+    expect(testHeap.extractMaximum()).toBeNull();
+    testHeap.insert(4);
+    testHeap.insert(5);
+    testHeap.insert(6);
+    testHeap.insert(3);
+    testHeap.insert(1);
+    testHeap.insert(2);
+    expect(testHeap.extractMaximum()).toEqual(6);
+    expect(testHeap.extractMaximum()).toEqual(5);
+    expect(testHeap.extractMaximum()).toEqual(4);
+    expect(testHeap.extractMaximum()).toEqual(3);
+    expect(testHeap.extractMaximum()).toEqual(2);
+    expect(testHeap.extractMaximum()).toEqual(1);
+
   });
 });
--- lab-andrew/model/max-heap.js
+++ lab-andrew/model/max-heap.js
@@ -4,6 +4,14 @@ class maxHeap {
   constructor(){
     this._data = [];
   }
+  
+  peek(){
+    if (!this._data.length){
+      return null;
+    }
+  
+    return this._data[0];
+  }
 
   insert(value){
     this._data.push(value);
@@ -33,12 +41,45 @@ class maxHeap {
     this._data[j] = temp;
   }
 
-  peek(){
-    if (this._data.length === 0){
+  extractMaximum() {
+    if (!this._data.length)
       return null;
+
+    let max = this._data[0];
+    let lastValue = this._data.pop();
+    if (this._data.length){
+      this._data[0] = lastValue;
+      this._bubbleDown(0);
+    }
+    return max;
+  }
+
+  _leftChildIndex(index) {
+    return (2 * index) + 1;
+  }
+
+  _rightChildIndex(index) {
+    return (2 * index) + 2;
+  }
+
+  _bubbleDown(index) {
+    let maxIndex = index;
+    let leftIndex = this._leftChildIndex(index);
+    let rightIndex = this._rightChildIndex(index);
+
+    if (leftIndex <= this._data.length - 1) {
+      if (this._data[maxIndex] < this._data[leftIndex])
+        maxIndex = leftIndex;
+    }
+    if (rightIndex <= this._data.length - 1) {
+      if (this._data[maxIndex] < this._data[rightIndex])
+        maxIndex = rightIndex;
     }
 
-    return this._data[0];
+    if (maxIndex !== index) {
+      this._swap(index, maxIndex);
+      this._bubbleDown(maxIndex);
+    }
   }
 }
 
