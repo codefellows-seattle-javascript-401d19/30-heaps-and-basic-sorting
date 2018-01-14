b94b784423b2c007f18d8aad718b0f3610b905c6
Andrew Bloom <blooma116@gmail.com>
2018-01-13 23:42:00 -0800

    refactored tests to use peek function

--- lab-andrew/__test__/max-heap.test.js
+++ lab-andrew/__test__/max-heap.test.js
@@ -5,11 +5,12 @@ const MaxHeap = require('../model/max-heap');
 describe('tests for max-heap.js', () => {
   test('testing that max-heap stores maximum value in root position', () => {
     let testHeap = new MaxHeap();
+    expect(testHeap.peek()).toBeNull();
     testHeap.insert(5);
-    expect(testHeap._data[0]).toEqual(5);
+    expect(testHeap.peek()).toEqual(5);
     testHeap.insert(3);
-    expect(testHeap._data[0]).toEqual(5);
+    expect(testHeap.peek()).toEqual(5);
     testHeap.insert(6);
-    expect(testHeap._data[0]).toEqual(6);
+    expect(testHeap.peek()).toEqual(6);
   });
 });
