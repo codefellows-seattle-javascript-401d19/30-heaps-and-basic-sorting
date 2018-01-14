0a812c43810540d1566df850ff43f6d8e0654ebe
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2018-01-13 16:09:21 -0800

    Three MaxHeap tests working just fine

--- lab-kerry/__test__/max-heap.test.js
+++ lab-kerry/__test__/max-heap.test.js
@@ -4,30 +4,52 @@ let MaxHeap = require('../model/max-heap');
 
 let Heap = new MaxHeap();
 
-Heap.insert(100);
-Heap.insert(19);
-Heap.insert(36);
-Heap.insert(25);
-Heap.insert(1);
-Heap.insert(17);
-Heap.insert(3);
-Heap.insert(2);
-Heap.insert(7);
-Heap.insert(32);
-Heap.insert(110);
-
-
-console.log('MaxHeap', Heap);
-
-console.log('Max value of heap', Heap.extractMaximum());
-
+let heapFactory = () => {
+	Heap.insert(100);
+	Heap.insert(19);
+	Heap.insert(36);
+	Heap.insert(25);
+	Heap.insert(1);
+	Heap.insert(17);
+	Heap.insert(3);
+	Heap.insert(2);
+	Heap.insert(7);
+	Heap.insert(32);
+	Heap.insert(110);
+};
 
 describe('Testing MaxHeap and its public methods', () => {
 	
-	describe('MaxHeap creation and insert', () => {
-		test('MaxHeap should be created and insert should place greater values higher in the tree.', () => {
+	
+	describe('MaxHeap creation/insertion', () => {
+		beforeEach(() => {
+			heapFactory();
+		});
 
-		})
+		afterEach(() => { 
+			return Heap._data = []
+		});
+
+		test('MaxHeap should be created and abide by the rules of a MaxHeap.', () => {
+			let expected = [110, 100, 36, 19, 32, 17, 3, 2, 7, 1, 25];
+			expect(Heap._data).toEqual(expected);
+			expect(Heap._data).toBeInstanceOf(Array);
+			expect(Heap._data).toHaveLength(11);
+		});
+
+		test('Upon insertion of new greatest value, heap should reorganize to abide by the rules of a MaxHeap.', () => {
+			Heap.insert(111);
+			let expected = [111, 100, 110, 19, 32, 36, 3, 2, 7, 1, 25, 17];
+			expect(Heap._data).toEqual(expected);
+			expect(Heap._data).toBeInstanceOf(Array);
+			expect(Heap._data).toHaveLength(12);
+		});
+
+		test('MaxHeap should throw error if no value passed.', () => {
+			expect(() => {
+				Heap.insert();
+			}).toThrow();
+		});
 	})
 })
 
