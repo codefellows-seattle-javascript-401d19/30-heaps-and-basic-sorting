f2f54f2ba4c6f2204123093665447784779d7d6b
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-01-13 20:04:59 -0800

    add peek and getParentIndex methods to max heap

--- lab-shannon/model/max-heap.js
+++ lab-shannon/model/max-heap.js
@@ -4,6 +4,21 @@ class maxHeap{
   constructor(){
     this._data = [];
   }
+
+  peek(){
+    if(this._data.length === 0){
+      return null;
+    }
+    return this._data[0];
+  }
+
+  _getParentIndex(index){
+    if(index === 0){
+      return null;
+    }
+    let parentIndex = Math.floor((index - 1) / 2);
+    return parentIndex;
+  }
 }
 
 module.exports = maxHeap;
