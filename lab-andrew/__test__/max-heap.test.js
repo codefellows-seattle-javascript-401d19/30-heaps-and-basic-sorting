3b39f714f6876d377ed9592014008426da28053c
Andrew Bloom <blooma116@gmail.com>
2018-01-13 23:39:51 -0800

    insert on max-heap works and tests pass

--- lab-andrew/__test__/max-heap.test.js
+++ lab-andrew/__test__/max-heap.test.js
@@ -6,10 +6,10 @@ describe('tests for max-heap.js', () => {
   test('testing that max-heap stores maximum value in root position', () => {
     let testHeap = new MaxHeap();
     testHeap.insert(5);
-    expect(testHeap.root.value).toEqual(5);
+    expect(testHeap._data[0]).toEqual(5);
     testHeap.insert(3);
-    expect(testHeap.root.value).toEqual(5);
+    expect(testHeap._data[0]).toEqual(5);
     testHeap.insert(6);
-    expect(testHeap.root.value).toEqual(6);
+    expect(testHeap._data[0]).toEqual(6);
   });
 });
--- lab-andrew/model/max-heap.js
+++ lab-andrew/model/max-heap.js
@@ -5,7 +5,41 @@ class maxHeap {
     this._data = [];
   }
 
-  
+  insert(value){
+    this._data.push(value);
+    this._bubbleUp(this._data.length - 1);
+  }
+
+  _bubbleUp(index) {
+    if (!index)
+      return;
+
+    let parentIndex = this._parentIndex(index);
+
+    if (this._data[parentIndex] < this._data[index]) {
+      this._swap(parentIndex, index);
+      this._bubbleUp(parentIndex);
+    }
+  }
+
+  _parentIndex(index) {
+    return Math.floor((index - 1) / 2);
+  }
+
+  _swap(i, j) {
+    let temp = this._data[i];
+
+    this._data[i] = this._data[j];
+    this._data[j] = temp;
+  }
+
+  peek(){
+    if (this._data.length === 0){
+      return null;
+    }
+
+    return this._data[0];
+  }
 }
 
 module.exports = maxHeap;
