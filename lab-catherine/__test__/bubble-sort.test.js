4b3a7035f77d1cb77bf26042ef945d2a02fe9974
Catherine Looper <loopercat@gmail.com>
2018-01-15 20:15:12 -0800

    Added a second test for bubble sort input validation

--- lab-catherine/__test__/bubble-sort.test.js
+++ lab-catherine/__test__/bubble-sort.test.js
@@ -3,6 +3,10 @@
 const bubbleSort = require('../lib/bubble-sort');
 
 describe('bubble-sort.js', () => {
+  test('testing that bubble sort method should throw an error if the input is not an array', () => {
+    expect(() => bubbleSort('notAnArray')).toThrow();
+  });
+  
   test('bubble sort method should return a sorted array', () => {
     const arrayToSort = [8, 2, 7, 25, 3, 6, 4];
     
