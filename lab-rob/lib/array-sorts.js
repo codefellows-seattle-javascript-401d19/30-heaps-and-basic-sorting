9e7a1df11fa7b7f613143feabcc553dcad7d4b87
Robert Reed <robert.mc.reed@gmail.com>
2018-01-15 21:55:25 -0800

    bubbleSort and insertionSort both passing

---
+++ lab-rob/lib/array-sorts.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const arraySorts = module.exports = {};
+
+arraySorts.bubbleSort = array => {
+  for(let i = 0; i < array.length - 1; i++) {
+    for(let j = 0; j < array.length - 1 - i; j++) {
+      if(array[j] > array[j + 1])
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+    }
+  }
+
+  return array;
+};
+
+arraySorts.insertionSort = array => {
+  for(let i = 1; i < array.length; i++) {
+    for(let j = i; j > 0; j--) {
+      if(array[j] < array[j - 1])
+        [array[j], array[j - 1]] = [array[j - 1], array[j]];
+      else
+        break;
+    }
+  }
+
+  return array;
+};
